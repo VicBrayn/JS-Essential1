7057da955bba9c98a9ea7abf9a47991350a7c8ef
VicBrayn <victoria.brayn@amalitech.org>
2020-10-10 14:28:47 +0100

    loged cssProperty and cssValue to console

--- vicapp.js
+++ vicapp.js
@@ -1,6 +1,13 @@
 function Set () 
 {
     let elements = document.getElementsByName('cssProperty');
+    for (let index=o;index<elements.length;index++)
+    {
+        let cssProperty = (elements[index].getAttribute('id'));
+        let cssValue = (elements[index].value);
+        console.log (cssProperty,cssValue);
+    }
+
 }
 
-document.getElementById('set')addEventListener('click',set);
+document.getElementById('set').addEventListener('click',set);
