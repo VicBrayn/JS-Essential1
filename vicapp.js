45a37eada05244eb81235c1bdc491e62f973e71c
VicBrayn <victoria.brayn@amalitech.org>
2020-10-11 00:26:50 +0100

    Editing typos

--- vicapp.js
+++ vicapp.js
@@ -7,17 +7,23 @@ element.onclick = function (){alert('hello');};
 document.body.appendChild (element);
 
 
-let elements = document.getElementsByName('cssProperty'); 
-let div = document.getElementsById('modify');
-function Set () 
+
+    let elements = 
+    document.getElementsByName('cssProperty');
+    console.log(elements); 
+
+    let div = document.getElementsById('modify');
+
+    function Set () 
 {
-   
-    for (let index=o;index<elements.length;index++)
+    for (let index =0; index<elements.length; index++)
     {
-        let cssProperty = (elements[index].getAttribute('id'));
-        let cssValue = (elements[index].value);
-       
-        div.style[cssProperty] = cssValue;
+        let cssProperty = (elements[ index ].getAttribute('id'));
+
+        let cssValue = (elements[ index ].value);
+   
+        div.style[ cssProperty ] = cssValue;
     }
 }
-document.getElementById('set').addEventListener('click',set);
+ 
+document.getElementById('set').addEventListener('click', set);
